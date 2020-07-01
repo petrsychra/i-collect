621869d4a74c6d39e94e527a6cd98d28f615e9d6
Petr <petrsychra@me.com>
2020-07-01 12:52:08 +0200

    add dev debugging pack

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -4,12 +4,17 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\Routing\Annotation\Route;
+
 /**
  * 
  */
 class AboutController
 {
 	
+	/**
+	* @Route("/abouts")
+	*/
 	public function number(): Response
 	{
 		$number = random_int(0, 100);
